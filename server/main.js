a6082d9e3b1024cbd2bc0204addae000a3bcbf28
sveinburne <theswanted@gmail.com>
2016-05-15 16:32:06 -0400

    importation collections serveur

--- server/main.js
+++ server/main.js
@@ -1,7 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
 import comments from '../imports/api/comments.js';
-import definitions from '../imports/api/definitions.js';
+import ccDefinitions from '../imports/api/ccDefinitions.js';
+import ccVisions from '../imports/api/ccVisions.js';
+import ccReferences from '../imports/api/ccReferences.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
